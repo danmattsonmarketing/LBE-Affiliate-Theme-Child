d59bca775afeebf3018c8e5879542c9616859d86
Dan Mattson <danmattsonmarketing@gmail.com>
2018-01-22 12:11:47 +1030

    1.7.1

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 require 'theme_update_check.php';
 $MyUpdateChecker = new ThemeUpdateChecker(
     'womness',
-    'https://kernl.us/api/v1/theme-updates/5a615b8ad3a06a26c3e3d01e/'
+    'https://kernl.us/api/v1/theme-updates/5a653f8f2822ed62e077c03a/'
 );
 // $MyUpdateChecker->license = "aKernlLicenseKey";  <---- optional
 // $MyUpdateChecker->remoteGetTimeout = 5; <---- optional
